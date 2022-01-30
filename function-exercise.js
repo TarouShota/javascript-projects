9d484198f55f9275643b62b9fa3641bf00b797f7
TarouShota <adilovbekzot@gmail.com>
2022-01-30 22:10:05 +0100

    done with every functions exercise

---
+++ function-exercise.js
@@ -0,0 +1,41 @@
+//Exercise - Minimum
+let greaterValue = (a, b) => {
+    switch (a, b) {
+        case a > b:
+            return a;
+        case a < b:
+            return b;
+        default:
+            return a;
+    }
+}
+console.log(greaterValue(1000, -9));
+//Exercise - Recursion
+//Requirement => Zero is even, one is odd, for any other number N, its evenness is the same as N-2
+let isEven = (number) => {
+    if (number == 0) {
+        return true;
+    } else if (number > 0) {
+        return isEven(number - 2);
+    } else if (number != -1) {
+        return isEven(number + 2);
+    } else {
+        return false
+    }
+}
+console.log(isEven(14));
+
+//Exercise - Bean Counting
+//Requirement 
+let countCharacters = (string, character) => {
+    let length = 0;
+    let characterCounter = 0;
+    while (string.length != length) {
+        if (string[length] == character) {
+            characterCounter++;
+            length++;
+        } else {
+            length++;
+        }
+    } return characterCounter;
+}
