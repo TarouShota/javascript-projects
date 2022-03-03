18c93abdc63635759a0816a1104020ad2e0d385a
TarouShota <adilovbekzot@gmail.com>
2022-03-03 23:38:08 +0100

    library-project

--- JavaScript_Projects/Objects_Syntax.js
+++ JavaScript_Projects/Objects_Syntax.js
@@ -13,31 +13,99 @@ let scoreBoard = {
 };
 
 let player1 = new HighScore('ben', '10');
+console.log(player1)
 
 // Create a ScoreBoard 
+
+
+//Adds a new player to a ScoreBoard
+
 let createScoreBoard = function (name, score) {
     if (name == undefined || score == undefined) {
         return scoreBoard
     }
-    scoreBoard.name = name;
-    scoreBoard.score = score;
+    scoreBoard[name] = score;
     return scoreBoard
 }
-//Adds a new player to a ScoreBoard
 
-let addPlayer = function (prevScore, name, score) {
-    let result = {
-        previouScore: (scoreBoard),
-        CurrentScore: {
-            'name': name,
-            'score': score
-        }
-    };
-    let s = `${Object.values(result.previouScore)} and ${(result.CurrentScore)}`;
-    return s
+// Adds a new player to a ScoreBoard, assigns prevScore to a scoreboard
+let addPlayer = function (prevScore, names, score) {
+    scoreBoard[names] = score;
+    Object.assign(scoreBoard, prevScore);
+    return scoreBoard
 }
 
-console.log(player1);
-console.log(player1)
-console.log(createScoreBoard('ben', 322));
-console.log(addPlayer(scoreBoard, 'Carlos', 123))
+// Removes a player from an object
+
+let removePlayer = function (existingScore, scoreToRemove) {
+    delete scoreBoard[scoreToRemove];
+}
+// Updates existing value in o
+let updateScore = function (player, newScore) {
+    scoreBoard[player] += newScore;
+}
+
+// Adds 100 to a every value in an object 
+
+let applyMondayBonus = function (theScoreBoard) {
+    for (let value in theScoreBoard) {
+        theScoreBoard[value] += 100;
+    }
+
+}
+// Passes an object to a set function
+
+let normalizeScore = function (objToNormal) {
+    return objToNormal.normalizeScore(objToNormal.score)
+}
+let normalize = (score) => {
+    return score + 100
+}
+
+
+
+const params = { score: 400, normalizeScore: normalize };
+console.log(normalizeScore(params));
+
+console.log(scoreBoard);
+
+
+
+// Linked List
+
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+let sumOfValues = 0;
+
+let printList = (list) => {
+    sumOfValues += list.value
+    if (list.next != null) {
+        printList(list.next);
+    }
+    return sumOfValues
+}
+
+let printListLoop = (list) => {
+    while (list.next != null) {
+        sumOfValues += list.value
+        list = list.next
+    }
+}
+
+
+
+console.log(printList(list));
+console.log(list.hasOwnProperty(''));
+console.log(list.next);
--- scoar-board-solution.js
+++ scoar-board-solution.js
@@ -1,20 +1,9 @@
-//scoar-board-solution
-function HighScore(name, score) {
-    this.name = name;
-    this.score = score;
-    this.delete = function () {
-        this.name = null;
-        this.score = 0;
-    }
-}
-// Create a ScoreBoard 
+
+//Intializing scoreBoard with default value
+
 let scoreBoard = {
     ['The Best Ever']: 100000
 };
-scoreBoard.chupapi = 123213;
-console.log(scoreBoard)
-
-let player1 = new HighScore('ben', '10');
 
 //Adds a new player to a ScoreBoard
 
@@ -26,30 +15,34 @@ let createScoreBoard = function (name, score) {
     return scoreBoard
 }
 
-//Adds a new player to a ScoreBoard, assigns prevScore to a scoreboard
+// Adds a new player to a ScoreBoard, assigns prevScore to a scoreboard
+
 let addPlayer = function (prevScore, names, score) {
     scoreBoard[names] = score;
     Object.assign(scoreBoard, prevScore);
     return scoreBoard
 }
+
 // Removes a player from an object
+
 let removePlayer = function (existingScore, scoreToRemove) {
     delete scoreBoard[scoreToRemove];
 }
-//Updates existing value in o
+// Updates existing value in o
 let updateScore = function (player, newScore) {
     scoreBoard[player] += newScore;
 }
 
-//Adds 100 to a every value in an object 
+// Adds 100 to a every value in an object 
+
 let applyMondayBonus = function (theScoreBoard) {
     for (let value in theScoreBoard) {
-        console.log(value);
         theScoreBoard[value] += 100;
     }
-    console.log(theScoreBoard);
+
 }
-//Passes an object to a set function
+// Passes an object to a set function
+
 let normalizeScore = function (objToNormal) {
     return objToNormal.normalizeScore(objToNormal.score)
 }
@@ -57,11 +50,3 @@ let normalize = (score) => {
     return score + 100
 }
 
-console.log(createScoreBoard('ben', 322));
-console.log(addPlayer({ 'Dave Thomas': 0 }, 'José Valim', 486373));
-applyMondayBonus(scoreBoard);
-
-
-const params = { score: 400, normalizeScore: normalize };
-console.log(normalizeScore(params));
-console.log(scoreBoard);
