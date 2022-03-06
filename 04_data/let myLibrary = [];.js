bbf00e7b719bc18a0f5fae7ad91e8ae3e39f7df3
TarouShota <adilovbekzot@gmail.com>
2022-03-06 20:02:41 +0100

    library:done

--- 04_data/let myLibrary = [];.js
+++ 04_data/let myLibrary = [];.js
@@ -1,16 +1,18 @@
 let myLibrary = [];
 
-
+const wholeSection = document.querySelector('.whole-section')
 const bookShelf = document.querySelector('.book-shelf');
 const bookTitle = document.querySelector('.book-title');
+const bookSection = document.querySelector('.book-section');
+
 
 
 const bookAuthor = document.querySelector('.book-author');
 const bookPages = document.querySelector('.book-pages');
 
-
-
 const bookState = document.querySelector('.book-state');
+
+
 
 function Book(title, author, pages, state) {
     this.title = title
@@ -24,11 +26,23 @@ function Book(title, author, pages, state) {
 
 Book.prototype = {
     addBookToLibrary: function () {
-        myLibrary.push(this)
-        bookTitle.textContent = this.title;
-        bookAuthor.textContent = this.author;
-        bookPages.textContent = this.pages;
-        bookState.textContent = this.state;
+        let div = document.createElement('div');
+        let ul = document.createElement('ul');
+
+        for (i = 1; i < 5; i++) {
+            let list = document.createElement('li');
+            ul.appendChild(list);
+        }
+        div.appendChild(ul);
+        wholeSection.append(div);
+        let lists = ul.children;
+
+        /* 
+ */
+        lists[0].textContent = this.title;
+        lists[1].textContent = this.author;
+        lists[2].textContent = this.pages;
+        lists[3].textContent = this.state;
     }
 }
 
@@ -37,23 +51,18 @@ Book.prototype = {
 const got = new Book('Game of Thrones', 'Allah', 295, 'not read yet');
 const aot = new Book('attack on titan', 'kageyama', 245, 'not read yet')
 //const berserk = new Book(prompt("Book Title:"), prompt('The Author:'), prompt('Pages:'), prompt("have u already read it?"));
-got.addBookToLibrary();
-console.log(myLibrary);
-aot.addBookToLibrary()
-
-
-
-
-const btn = document.querySelector('#btn');
-btn.onclick = () => {
-    const newBook = new Book(prompt("Book Title:"), prompt('The Author:'), prompt('Pages:'), prompt("have u already read it?"));
-    newBook.addBookToLibrary()
-};
-
-got.addBookToLibrary()
 console.log(myLibrary);
 
-aot.addBookToLibrary();
-console.log(myLibrary);
 
-console.log(got);
+
+
+try {
+    const btn = document.querySelector('#btn');
+    btn.onclick = () => {
+        const newBook = new Book(prompt("Book Title:"), prompt('The Author:'), prompt('Pages:'), prompt("have u already read it?"));
+        newBook.addBookToLibrary()
+    };
+} catch {
+    console.log('eei')
+}
+
