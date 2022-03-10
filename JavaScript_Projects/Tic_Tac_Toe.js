c585b462a1ff906379296e3032a05ab7e0848492
TarouShota <adilovbekzot@gmail.com>
2022-03-10 20:31:51 +0100

    game-over-state

--- JavaScript_Projects/Tic_Tac_Toe.js
+++ JavaScript_Projects/Tic_Tac_Toe.js
@@ -1,5 +1,15 @@
 let game = {
     gameBoard: ['', '', '', '', '', '', '', '', ''],
+    winningMoves:
+        [[0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]],
+
     counter: 0,
     updateArray() {
         this.counter++
@@ -12,33 +22,58 @@ let game = {
     gameState() {
         this.first = this.gameBoard.slice(0, 3);
         this.second = this.gameBoard.slice(3, 6)
+
         this.third = this.gameBoard.slice(6, 9);
-        this.twoD = [this.first, this.second, this.third]
-        return this.twoD[0].join('');
+        this.twoD = [this.first, this.second, this.third];
+
+
+        return false
+        console.log(this.twoD);
+        //return this.twoD[0].join('');
         switch (this) {
             case this.gameBoard.slice():
         }
     },
-    gameOver() {
-        mainDisplay.classList.add('blur-effect')
-        frontDisplay.classList.add('display-flex')
+    gameOver(value) {
+        mainDisplay.classList.add('blur-effect');
+        frontDisplay.classList.add('display-flex');
+        return result.textContent = `${value} Wins!`
+
+
+    },
+    fullArray() {
+        return this.gameBoard.every(function (e) {
+            return e > 'A';
+        });
 
     },
     gameStart: function (node) {
 
-        if (this.gameBoard[node] != "X" && this.gameBoard[node] != "O") {
-            if (this.gameboardString.length != 1) {
-                this.asignValue(node);
-                this.updateArray()
-            } else {
-                this.asignValue(node);
-                this.gameOver();
+        if (this.gameBoard[node] < 'A') {
+            if (this.gameState() == true) {
+                return true
             }
+            else if (this.gameState() == false && this.fullArray() == false) {
+                this.asignValue(node);
+                this.updateArray();
+
+                if (this.fullArray() == true) {
+                    this.asignValue(node);
+                    return this.gameOver((this.counter % 2 == 0) ? "O" : "X");
+                }
+            }
+
+
         }
     }
 }
 game.gameboardString = 0;
 
+
+
+
+
+//console.log(resulting);
 
 
 let theButton = document.querySelector('button');
@@ -47,9 +82,10 @@ let mainDisplay = document.querySelector('main');
 
 let body = document.querySelector('body');
 let frontDisplay = body.firstElementChild;
-let header = frontDisplay
+let result = frontDisplay.querySelector('h2');
 
 
+console.log(game.gameState());
 
 
 
