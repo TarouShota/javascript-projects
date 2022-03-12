a50507ce5037a58df67e120132811c0416fe3d64
TarouShota <adilovbekzot@gmail.com>
2022-03-12 14:01:24 +0100

    more readable code

--- tic-tac-toe-game/Tic_Tac_Toe.js
+++ tic-tac-toe-game/Tic_Tac_Toe.js
@@ -21,7 +21,7 @@ let game = {
         body.appendChild(whoStarts);
 
         let whoStartsText = document.createElement('h1');
-        // let hideWhoStarts = () => { return whoStarts.classList.add('hide-display') };
+        let hideWhoStarts = () => { return whoStarts.classList.add('hide-display') };
 
         whoStarts.appendChild(whoStartsText);
 
@@ -32,12 +32,13 @@ let game = {
 
 
 
-        return setTimeout(this.hideWhoStarts, 1000,);
+        return setTimeout(hideWhoStarts, 1000,);
     },
-    hideWhoStarts() { return whoStarts.classList.add('hide-display') },
+
     gameOver(value) {
         mainDisplay.classList.add('blur-effect');
         frontDisplay.classList.add('display-flex');
+
         return result.textContent = `${value} wins!`
 
 
@@ -54,19 +55,24 @@ let game = {
             element.forEach(element => {
                 return tempArray.push(game.gameBoard[element]);
             });
+
             let checkForX = tempArray.every(function (e) {
                 return e == 'X'
             })
+
             let checkForO = tempArray.every(function (e) {
                 return e == 'O'
             })
+
             if (checkForX == true) {
                 this.winnerDeclared = true
                 return game.gameOver(this.player2);
-            } else if (checkForO == true) {
+            }
+            else if (checkForO == true) {
                 this.winnerDeclared = true
                 return game.gameOver(this.player1);
-            } else {
+            }
+            else {
                 this.winnerDeclared = false
                 return console.log(this.fullArray());
             }
@@ -82,6 +88,7 @@ let game = {
 
         if (this.gameBoard[node] < 'A') {
             this.counter++
+
             this.asignValue(node);
             this.winState();
 
@@ -102,17 +109,22 @@ let game = {
 let theButton = document.querySelector('button');
 let mainDisplay = document.querySelector('main');
 
+
 let body = document.querySelector('body');
 let frontDisplay = body.firstElementChild;
+
 
 let result = frontDisplay.querySelector('h2');
 let welcomeSection = document.createElement('section');
 
+
 let firstPlayer = document.createElement('form');
 let secondPlayer = document.createElement('form')
-let secondPlayerInp = document.createElement('input');
 
+
+let secondPlayerInp = document.createElement('input');
 let submitButton = document.createElement('button');
+
 
 submitButton.setAttribute('type', 'submit');
 submitButton.textContent = "Start";
@@ -122,65 +134,58 @@ firstPlayerInp = document.createElement('input');
 
 
 firstPlayerInp.setAttribute
-let inputValues = [
-    'type', 'text', 'id', 'player1', 'placeholder', 'Player - O', 'class', 'form-input'
-]
+let inputValues = ['type', 'text', 'id', 'player1', 'placeholder', 'Player - O', 'class', 'form-input'];
+
+
 console.log(inputValues[7]);
-//
+
+
 let temp = 1;
 for (i = 0; i < 4; i += 1) {
     firstPlayerInp.setAttribute(`${inputValues.shift()}`, `${inputValues.shift()}`);
 }
+
+
 secondPlayerInp = firstPlayerInp.cloneNode(true);
 secondPlayerInp.setAttribute('id', 'player2');
+
+
 secondPlayerInp.setAttribute('placeholder', 'Player - X');
-
-
-
 
 
 firstPlayerInp.setAttribute('style', 'color:white; background:black; width:10rem; height:3rem');
 secondPlayerInp.setAttribute('style', 'color:white; background:black; width:10rem; height:3rem');
 
+
 firstPlayer.appendChild(firstPlayerInp);
 secondPlayer.appendChild(secondPlayerInp);
 
+
 welcomeSection.append(firstPlayer, secondPlayer);
 welcomeSection.appendChild(submitButton);
+
+
 welcomeSection.setAttribute('class', 'player-info');
-
-
 body.appendChild(welcomeSection);
 
 
 for (i = 0; i < 9; i++) {
     const div = document.createElement('div')
     div.setAttribute('id', `${i}`);
+
     mainDisplay.appendChild(div);
 };
+
+
 submitButton.addEventListener('click', (event) => {
     game.player1 = firstPlayerInp.value;
     game.player2 = secondPlayerInp.value;
+
     game.showBoard();
-
-
 });
 
+
 let mainSection = [...document.querySelectorAll('div')];
-
-
-
-//console.log(resulting);
-
-
-
-
-
-
-
-
-
-//console.log(game.gameState())
 
 mainSection.forEach((button) => {
 
