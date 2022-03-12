1521f098e4b90894c42ee14d88ca6760e9dcfce8
TarouShota <adilovbekzot@gmail.com>
2022-03-12 15:32:39 +0100

    commenting a code for a better understanding

--- tic-tac-toe-game/Tic_Tac_Toe.js
+++ tic-tac-toe-game/Tic_Tac_Toe.js
@@ -1,5 +1,6 @@
 
 let game = {
+    //gameboard array, set of winning moves
     gameBoard: ['', '', '', '', '', '', '', '', ''],
     winningMoves:
         [[0, 1, 2],
@@ -11,7 +12,10 @@ let game = {
         [0, 4, 8],
         [2, 4, 6]],
 
+    //counter to decide which element to place on a tile
+
     counter: 0,
+    //asigning value to an array, assing a text content to a div-tile from array
     asignValue(node) {
         this.gameBoard[node] = (this.counter % 2 == 0) ? "X" : "O";
         return mainSection[node].textContent = this.gameBoard[node];
@@ -21,6 +25,9 @@ let game = {
         body.appendChild(whoStarts);
 
         let whoStartsText = document.createElement('h1');
+
+        //shows which player starts, appers for 2 seconds, and dissappears
+
         let hideWhoStarts = () => { return whoStarts.classList.add('hide-display') };
 
         whoStarts.appendChild(whoStartsText);
@@ -31,8 +38,8 @@ let game = {
         whoStartsText.textContent = `${this.player1} starts !`;
 
 
-
-        return setTimeout(hideWhoStarts, 1000,);
+        //calls hidewhostarts function with a timeone 2 seconds
+        return setTimeout(hideWhoStarts, 2000,);
     },
 
     gameOver(value) {
@@ -43,12 +50,14 @@ let game = {
 
 
     },
+    //check if array is full
     fullArray() {
         return this.gameBoard.every(function (e) {
             return e > 'A';
         });
 
     },
+    //check if the there is a winning move
     winState() {
         game.winningMoves.forEach(element => {
             let tempArray = [];
@@ -84,6 +93,7 @@ let game = {
 
 
     },
+    //press on a tile
     gameStart: function (node) {
 
         if (this.gameBoard[node] < 'A') {
@@ -106,6 +116,7 @@ let game = {
 
 }
 
+//creating all of the neccessary html elements, using dom
 let theButton = document.querySelector('button');
 let mainDisplay = document.querySelector('main');
 
@@ -132,43 +143,40 @@ submitButton.textContent = "Start";
 
 firstPlayerInp = document.createElement('input');
 
-
+//all atributes, which will be passed to firstPlayerINp
 firstPlayerInp.setAttribute
 let inputValues = ['type', 'text', 'id', 'player1', 'placeholder', 'Player - O', 'class', 'form-input'];
 
 
-console.log(inputValues[7]);
-
-
-let temp = 1;
 for (i = 0; i < 4; i += 1) {
     firstPlayerInp.setAttribute(`${inputValues.shift()}`, `${inputValues.shift()}`);
 }
 
 
+//cloning firstPlayerInp to a second one
 secondPlayerInp = firstPlayerInp.cloneNode(true);
+
+//adding a diferentional values to a secondplayerinp
 secondPlayerInp.setAttribute('id', 'player2');
-
-
 secondPlayerInp.setAttribute('placeholder', 'Player - X');
 
-
+//adding a style attribute
 firstPlayerInp.setAttribute('style', 'color:white; background:black; width:10rem; height:3rem');
 secondPlayerInp.setAttribute('style', 'color:white; background:black; width:10rem; height:3rem');
 
-
+//apending inputs to a form
 firstPlayer.appendChild(firstPlayerInp);
 secondPlayer.appendChild(secondPlayerInp);
 
-
+//appending form to a section
 welcomeSection.append(firstPlayer, secondPlayer);
 welcomeSection.appendChild(submitButton);
 
 
-welcomeSection.setAttribute('class', 'player-info');
+//appending section to a body
 body.appendChild(welcomeSection);
 
-
+// loop to create div elements
 for (i = 0; i < 9; i++) {
     const div = document.createElement('div')
     div.setAttribute('id', `${i}`);
@@ -176,6 +184,7 @@ for (i = 0; i < 9; i++) {
     mainDisplay.appendChild(div);
 };
 
+//event listener to a sumbit button
 
 submitButton.addEventListener('click', (event) => {
     game.player1 = firstPlayerInp.value;
@@ -185,7 +194,12 @@ submitButton.addEventListener('click', (event) => {
 });
 
 
+//select all divs
+
 let mainSection = [...document.querySelectorAll('div')];
+
+//every tile-div gets assigned a click event
+//passes it's div id to gameStart function
 
 mainSection.forEach((button) => {
 
