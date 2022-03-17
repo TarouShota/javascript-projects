2dc5258dbdd707182071bb8aea68baee847b7429
TarouShota <adilovbekzot@gmail.com>
2022-03-17 16:00:29 +0100

    using prettier on tic-tac-toe.js

---
+++ library-project/src/classes.js
@@ -0,0 +1,34 @@
+import { myLibrary, wholeSection, bookShelf, bookTitle, bookSection, bookAuthor, bookPages, bookState, counter } from './definingVariables.js';
+
+
+class Book {
+    constructor(title, author, pages, state) {
+        this.title = title
+        this.author = author
+
+        this.pages = pages
+        this.state = state
+    }
+
+    addBookToLibrary() {
+        let div = document.createElement('div');
+        let ul = document.createElement('ul');
+
+        for (let i = 0; i < 4; i++) {
+            let list = document.createElement('li');
+            ul.appendChild(list);
+            console.log("SUUi")
+        }
+        div.appendChild(ul);
+        wholeSection.append(div);
+        let lists = ul.children;
+
+        lists[0].textContent = this.title;
+        lists[1].textContent = this.author;
+        lists[2].textContent = this.pages;
+        lists[3].textContent = this.state;
+    }
+    //this.book = (`${title} by ${author}, ${pages} pages, ${state}`)
+}
+
+export { Book, myLibrary, wholeSection, bookShelf, bookTitle, bookSection, bookAuthor, bookPages, bookState, counter };
