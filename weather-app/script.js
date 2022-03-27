44b89bbed4a5f19f9effd0eace0616a6f9126d4c
TarouShota <adilovbekzot@gmail.com>
2022-03-27 18:18:15 +0200

    finished weather app

--- weather-app/script.js
+++ weather-app/script.js
@@ -16,29 +16,26 @@ let createSearchForm = function (appendIn = document.body, btnValue = 'search') 
 createSearchForm(document.body, 'eat it');
 const img = document.querySelector('img');
 
+let styleForms = () => {
+    searchValue.setAttribute(
+        "style",
+        "color:white; background:black; width:10rem; height:3rem"
+    );
 
+    img.setAttribute(
+        'style', 'height:40vh;widht:100vw'
+    );
+}
+styleForms();
 
 /*Fetching the gif from giphy api on and setting the searched value to empty*/
 
 
 searchBtn.addEventListener("click", () => {
-    fetchGif(searchValue.value);
-    searchValue.value = ''
+    showWeather(searchValue.value);
+    //searchValue.value = ''
 });
 
-
-
-/* This is setting the style of the input element. */
-
-
-searchValue.setAttribute(
-    "style",
-    "color:white; background:black; width:10rem; height:3rem"
-);
-
-img.setAttribute(
-    'style', 'height:40vh;widht:100vw'
-);
 
 
 /* Fetching a gif from the Giphy API and setting the src attribute of an image element to the URL of
@@ -49,16 +46,32 @@ async function fetchGif(toSearch = 'dog') {
     const catData = await response.json();
     try {
         img.src = catData.data.images.original.url;
+        console.log(catData)
     } catch {
         console.log('smily face:)');
     }
 }
+async function showWeather(toSearch) {
+    img.src = `https://avatars.githubusercontent.com/u/46716452?v=4`;
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${toSearch}&limit=&appid=4e933858aef6891926f4927f0068c206`, { mode: 'cors' })
+    const coordinates = await response.json();
+    const [lon, lat] = [coordinates[0].lon, coordinates[0].lat];
 
-fetchGif();
+    const weatherFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4e933858aef6891926f4927f0068c206`, { mode: 'cors' })
+    const weatherData = await weatherFetch.json();
+    const weatherIcon = weatherData.weather[0].icon
 
-testCunt = () => {
-    window.test = 1;
+
+    try {
+        img.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+        console.log(coordinates);
+        console.log(weatherData);
+        console.log(weatherData.weather[0].icon)
+    } catch (error) {
+        console.log(error);
+    }
 }
-testCunt();
 
-console.log(test + 1);
+//fetchGif();
+
+
