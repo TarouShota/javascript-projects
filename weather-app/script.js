3bc9790d904b5146ed117d34448229a97b50680b
TarouShota <adilovbekzot@gmail.com>
2022-03-27 13:18:48 +0200

    creating global function, to insert form and submit button

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,23 +1,32 @@
 /* Creating a form element and setting the type of the submit button to submit. */
-let searchForm = document.createElement("form");
-const searchValue = document.createElement('input');
-const searchBtn = document.createElement("input");
+let createSearchForm = function (appendIn = document.body, btnValue = 'search') {
+    window.searchForm = document.createElement("form");
+    window.searchValue = document.createElement('input');
+    window.searchBtn = document.createElement("input");
 
+    searchBtn.setAttribute("type", "submit");
+    searchValue.setAttribute('type', 'text');
+
+    searchBtn.value = btnValue;
+
+    searchForm.append(searchValue);
+    appendIn.append(searchForm, searchBtn);
+
+}
+createSearchForm(document.body, 'eat it');
 const img = document.querySelector('img');
 
 
 
 /* Creating a form element and setting the type of the submit button to submit. */
-searchBtn.setAttribute("type", "submit");
-searchBtn.textContent = "Search";
 
 
 searchBtn.addEventListener("click", () => {
     showGif(searchValue.value);
     searchValue.value = ''
 });
-searchForm.appendChild(searchValue);
-document.body.append(searchBtn, searchForm);
+
+
 
 /* This is setting the style of the input element. */
 
@@ -26,12 +35,12 @@ searchValue.setAttribute(
     "style",
     "color:white; background:black; width:10rem; height:3rem"
 );
-searchValue.setAttribute('type', 'text');
+
 
 /* Fetching a gif from the Giphy API and setting the src attribute of an image element to the URL of
 the gif. */
 
-showGif = (toSearch) => {
+showGif = (toSearch = 'dog') => {
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=dug0j2a4gXaxE7Rid0exPLjC5GLJxVTL&s=${toSearch}`, { mode: 'cors' })
         .then(function (response) {
             console.log(response);
