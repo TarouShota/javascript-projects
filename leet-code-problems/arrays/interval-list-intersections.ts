896d698945adf6b89e071888949969a45758adbe
TarouShota <adilovbekzot@gmail.com>
2022-05-29 02:29:49 +0200

    leet-code-problem-two-pointer

---
+++ leet-code-problems/arrays/interval-list-intersections.ts
@@ -0,0 +1,6 @@
+function intervalIntersection(firstList: number[][], secondList: number[][]): number[][] {
+    return []
+};
+console.log(intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]], [[1, 5], [8, 12], [15, 24], [25, 26]]));
+
+//possible solution is to use Two pointer technique another is array.include method, which is yet not researched
