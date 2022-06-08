9f2bdc8bfc4cb14c4bdfe9ec25d173b57d03a2e3
TarouShota <adilovbekzot@gmail.com>
2022-06-08 10:25:26 +0200

    task list for saturday

--- leet-code-problems/arrays/interval-list-intersections.ts
+++ leet-code-problems/arrays/interval-list-intersections.ts
@@ -1,12 +1,18 @@
-function intervalIntersection(firstList: number[][], secondList: number[][]): number[][] {
+export function intervalIntersection(firstList: number[][], secondList: number[][]): number[][] {
     let i = 0;
     let j = 0;
-    for (i; i < firstList.length; i++) {
-
-    }
+    let maxList = (firstList.length > secondList.length) ? firstList : secondList;
+    console.log(maxList);
+    maxList.forEach((element, index) => {
+        for (i; i < element.length; i++) {
+            for (j; j <)
+        }
+    });
 };
 console.log(intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]], [[1, 5], [8, 12], [15, 24], [25, 26]]));
 
 
+
+//solve this on saturday
 //possible solution is to u
 //two for loops
