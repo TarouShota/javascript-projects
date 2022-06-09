57a72533da3257561afd25a896f72c377a2fdbaf
TarouShota <adilovbekzot@gmail.com>
2022-06-09 10:48:50 +0200

    tasks for a weekend

--- leet-code-problems/arrays/interval-list-intersections.ts
+++ leet-code-problems/arrays/interval-list-intersections.ts
@@ -12,7 +12,7 @@ export function intervalIntersection(firstList: number[][], secondList: number[]
 console.log(intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]], [[1, 5], [8, 12], [15, 24], [25, 26]]));
 
 
-
+//he he preact is coming up
 //solve this on saturday
 //possible solution is to u
 //two for loops
