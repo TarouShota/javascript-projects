40d9ff1cd5afb380e181f8aff054c127e235c839
TarouShota <adilovbekzot@gmail.com>
2022-06-13 19:15:11 +0200

    ayo

--- leet-code-problems/arrays/interval-list-intersections.ts
+++ leet-code-problems/arrays/interval-list-intersections.ts
@@ -29,3 +29,4 @@ console.log(intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]], [[1, 5],
 // just like this
 // Ok Aisha, yee not this week, but then u say oh ye okk
 // after meeting, went prety well, was listening and digesting everything
+// whats poppin
